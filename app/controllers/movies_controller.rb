0bf4f8a42afb3cbf2c807ffac6cad57c6284ca5e
btcline04 <btcline04@gmail.com>
2018-10-02 21:26:08 -0400

    Add movie form and styling

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,26 @@
 class MoviesController < ApplicationController
+  
   def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to movie_path(@movie)
+    else
+      flash[:danger] = "Please try again!"
+      redirect_to new_movie_path
+    end
+  end
+
+  private
+
+  def find_movie
+    @movie = Movie.find(params[:id])
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title, :year, :genre, :poster, :runtime, :director, :plot, :list_id)
   end
 end
--- db/migrate/20181002032450_create_list_movies.rb
+++
@@ -1,8 +0,0 @@
-class CreateListMovies < ActiveRecord::Migration[5.2]
-  def change
-    create_table :list_movies do |t|
-
-      t.timestamps
-    end
-  end
-end
