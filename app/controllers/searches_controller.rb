b19fc265e569c5e7bc64e2bd2a5ab41fce1c696c
btcline04 <btcline04@gmail.com>
2018-10-08 11:20:34 -0400

    Update search index to view JSON

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,12 +4,15 @@ class SearchesController < ApplicationController
   end
 
   def omdb
-    @conn = Faraday.new(:url => 'http://www.omdbapi.com')
+    conn = Faraday.new(:url => 'http://www.omdbapi.com')
 
-    @conn.get do |req|
+    @resp = conn.get do |req|
       req.params['apikey'] = ENV['OMDB_API_KEY']
       req.params['s'] = params[:movie]
     end
+
+    body_hash = JSON.parse(@resp.body)
+    @movies = body_hash['search']
     render 'index'
   end
 
